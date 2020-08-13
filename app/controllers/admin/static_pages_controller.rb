c2b779fe2493f4b05bb2a7f9341c726c3ba0b616
Jeanille <jeanillevenabayon30@gmail.com>
2020-08-13 16:33:50 +0800

    Created method and placed before_action in admin controller

--- app/controllers/admin/static_pages_controller.rb
+++ app/controllers/admin/static_pages_controller.rb
@@ -1,11 +1,8 @@
 class Admin::StaticPagesController < ApplicationController
 
-  before_action :logged_in_user, only: [:home]
+  before_action :admin_only, only:[ :home ]
+  before_action :logged_in_user, only: [ :home ] 
 
   def home
-    if !current_user.isAdmin
-      flash[:danger] = "You are not authorized to access the admin dashboard."
-      redirect_to user_url(current_user.id)
-    end
   end
 end
