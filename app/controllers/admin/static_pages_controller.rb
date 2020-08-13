89261ae56c8ba20be45d53a988f744cc303fd24c
Jeanille <jeanillevenabayon30@gmail.com>
2020-08-13 16:35:07 +0800

    added space in the before_action

--- app/controllers/admin/static_pages_controller.rb
+++ app/controllers/admin/static_pages_controller.rb
@@ -1,6 +1,6 @@
 class Admin::StaticPagesController < ApplicationController
 
-  before_action :admin_only, only:[ :home ]
+  before_action :admin_only, only: [ :home ]
   before_action :logged_in_user, only: [ :home ] 
 
   def home
