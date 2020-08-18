d829c7743dcc29c87f6dc0fc5cd84ab0ad78e540
Jeanille <jeanillevenabayon30@gmail.com>
2020-08-18 16:48:10 +0800

    Improved choice validations and UI logic

--- app/models/word.rb
+++ app/models/word.rb
@@ -8,15 +8,20 @@ class Word < ApplicationRecord
 
   #validations
   validates :content, presence: true, length: { minimum: 3 }
-  validate :choices_validations
+  validate :has_one_correct_choice
+  validate :unique_choices
 
-  def choices_validations
+  def has_one_correct_choice
     len = choices.select{|choice| choice.isCorrect }.length
     if len > 1
       return errors.add :base, "A Word can contain ONLY ONE correct answer!"
     elsif len == 0
       return errors.add :base, "A Word SHOULD HAVE ONE correct answer!"
-    elsif choices.select{|x| choices}.uniq {|c| [c.content.downcase]}.length < choices.length
+    end
+  end
+
+  def unique_choices
+    if choices.select{|x| choices}.uniq {|c| [c.content.downcase]}.length < choices.length
       return errors.add :base, "Choices should be unique."
     end
   end
