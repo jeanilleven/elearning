dc4790ec8ad5b32eb53ebf633abc3edbb945df6b
Jeanille <jeanillevenabayon30@gmail.com>
2020-08-18 16:13:06 +0800

    Added choice validations

--- app/models/word.rb
+++ app/models/word.rb
@@ -8,4 +8,16 @@ class Word < ApplicationRecord
 
   #validations
   validates :content, presence: true, length: { minimum: 3 }
+  validate :choices_validations
+
+  def choices_validations
+    len = choices.select{|choice| choice.isCorrect }.length
+    if len > 1
+      return errors.add :base, "A Word can contain ONLY ONE correct answer!"
+    elsif len == 0
+      return errors.add :base, "A Word SHOULD HAVE ONE correct answer!"
+    elsif choices.select{|x| choices}.uniq {|c| [c.content.downcase]}.length < choices.length
+      return errors.add :base, "Choices should be unique."
+    end
+  end
 end
