ba91168c2b275275834fe78245190447c4624809
Jeanille <jeanillevenabayon30@gmail.com>
2020-08-28 18:25:34 +0800

    Modified models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
   has_many :words, dependent: :destroy
-  has_many :lessons
+  has_many :lessons, dependent: :destroy
 
   validates :title, presence: true, length: { minimum: 3 }
   validates :description, presence: true, length: { minimum: 5 }
